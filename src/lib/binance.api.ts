5ca8c6f74aa74e8a121d2b95b38887ea62f3ca87
Fujisawa Koki <melumuccu413@gmail.com>
2021-12-12 16:07:23 +0900

    Refactor: BinanceApi#getAllBalances 更に関数型の記述になるよう修正

--- src/lib/binance.api.ts
+++ src/lib/binance.api.ts
@@ -24,30 +24,26 @@ export class BinanceApi {
   getAllBalances(minQuantity: number, includeOnOrder: boolean): Promise<ProcessedBalance[]> {
     return new Promise(resolve => {
       binance.balance((error: string, balances: Balance) => {
-        // 最小数量を超えた仮想通貨名を抽出(ex. [BTC, ETH, ...])
-        const cryptosOrMoreMinQuantity = Object.keys(balances).filter(crypto => {
+        /** 最小数量を超えた仮想通貨名を抽出(ex. [BTC, ETH, ...]) */
+        const orMoreMinQuantity = (crypto: string) => {
           const availableB = new BigNumber(parseFloat(balances[crypto].available));
           const onOrderB = new BigNumber(parseFloat(balances[crypto].onOrder));
           // 対象とする数量
           const targetB = includeOnOrder ? availableB.plus(onOrderB) : availableB;
           // 最小数量以上の通貨のみにfilter
           return targetB.toNumber() > minQuantity;
-        });
-        console.log(
-          'file: binance.api.ts => line 36 => cryptosOrMoreMinQuantity => cryptosOrMoreMinQuantity',
-          cryptosOrMoreMinQuantity
-        );
+        };
 
-        // memo: フロント側で扱いやすい形に変換している
-        return resolve(
-          cryptosOrMoreMinQuantity.map(crypto => {
-            return {
-              crypto,
-              available: balances[crypto].available,
-              onOrder: balances[crypto].onOrder,
-            };
-          })
-        );
+        /** フロント側で扱いやすい形に変換 */
+        const processing = (crypto: string) => {
+          return {
+            crypto,
+            available: balances[crypto].available,
+            onOrder: balances[crypto].onOrder,
+          };
+        };
+
+        return resolve(Object.keys(balances).filter(orMoreMinQuantity).map(processing));
       });
     });
   }
