b3b3e66b7fef4722816bbdbd3bb8b160f475a11e
Fujisawa Koki <melumuccu413@gmail.com>
2021-12-12 11:09:28 +0900

    Refactor: 関数っぽく書き換え_BinanceApi#getAllBalances

--- src/lib/binance.api.ts
+++ src/lib/binance.api.ts
@@ -7,38 +7,45 @@ import { Balance } from '../shared/types';
 const Binance = require('node-binance-api');
 const binance = new Binance().options(keyOfBinance);
 
+type ProcessedBalance = {
+  crypto: string;
+  available: string;
+  onOrder: string;
+};
+
 export class BinanceApi {
   /**
-   * 全ペアの現在保有額を取得
+   * 全通貨の現在保有額を取得
+   *
    * @param includeOnOrder 注文中の数量を含むか
    * @param binance
-   * @returns 全ペアの現在保有額
+   * @returns 全通貨の現在保有額
    */
-  getAllBalances(includeOnOrder: boolean): Promise<Balance> {
+  getAllBalances(includeOnOrder: boolean): Promise<ProcessedBalance[]> {
     return new Promise(resolve => {
-      const balanceOfHasCoins: Balance = {};
       binance.balance(function (error: string, balances: Balance) {
-        // 保有している通貨のみに限定
-        for (const balance in balances) {
-          const availableB = new BigNumber(parseFloat(balances[balance].available));
-          const onOrderB = new BigNumber(parseFloat(balances[balance].onOrder));
+        // 対象数量が0でない仮想通貨名(ex. [BTC, ETH, ...])
+        const over0Cryptos = Object.keys(balances).filter(crypto => {
+          const availableB = new BigNumber(parseFloat(balances[crypto].available));
+          const onOrderB = new BigNumber(parseFloat(balances[crypto].onOrder));
+          // 対象数量
+          const targetB = includeOnOrder ? availableB.plus(onOrderB) : availableB;
+          // 保有している通貨のみに限定
+          return targetB.toNumber() > 0;
+        });
+        console.log('対象数量が0でない仮想通貨: ', over0Cryptos);
 
-          let tmpBalanceB;
-          if (includeOnOrder) {
-            // 注文中の数量を含む
-            tmpBalanceB = availableB.plus(onOrderB);
-          } else {
-            // 注文中の数量を含まない
-            tmpBalanceB = availableB;
-          }
+        const balanceOfOver0Cryptos: ProcessedBalance[] = [];
+        over0Cryptos.map(over0Crypto => {
+          const processed: ProcessedBalance = {
+            crypto: over0Crypto,
+            available: balances[over0Crypto].available,
+            onOrder: balances[over0Crypto].onOrder,
+          };
+          balanceOfOver0Cryptos.push(processed);
+        });
 
-          if (tmpBalanceB.toNumber() !== 0) {
-            balanceOfHasCoins[balance] = balances[balance];
-            // console.log("file: binanceUtil.ts => line 130 => binance.balance => balanceOfHasCoins", balanceOfHasCoins);
-          }
-        }
-
-        return resolve(balanceOfHasCoins);
+        return resolve(balanceOfOver0Cryptos);
       });
     });
   }
